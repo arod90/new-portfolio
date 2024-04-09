dcfb0996a830eaa89c8ab01160524c517fbaf0c8
Andres <web.arod@gmail.com>
2024-04-09 02:52:15 -0500

    hero and toolkit sections completed

---
+++ app/Components/MovingCards/MovingCards.tsx
@@ -0,0 +1,144 @@
+'use client';
+
+import { cn } from '@/utils/cn';
+import React, { useEffect, useState } from 'react';
+
+export const MovingCards = ({
+  items,
+  direction = 'left',
+  speed = 'fast',
+  pauseOnHover = true,
+  className,
+}: {
+  items: {
+    // quote: string;
+    id: number;
+    name: string;
+    name2: string;
+    name3: string;
+    icon?: JSX.Element;
+    icon2?: JSX.Element;
+    icon3?: JSX.Element;
+    // name: string;
+  }[];
+  direction?: 'left' | 'right';
+  speed?: 'fast' | 'normal' | 'slow';
+  pauseOnHover?: boolean;
+  className?: string;
+}) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === 'left') {
+        containerRef.current.style.setProperty(
+          '--animation-direction',
+          'forwards'
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          '--animation-direction',
+          'reverse'
+        );
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === 'fast') {
+        containerRef.current.style.setProperty('--animation-duration', '20s');
+      } else if (speed === 'normal') {
+        containerRef.current.style.setProperty('--animation-duration', '40s');
+      } else {
+        containerRef.current.style.setProperty('--animation-duration', '80s');
+      }
+    }
+  };
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        'scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]',
+        className
+      )}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          ' flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap',
+          start && 'animate-scroll ',
+          pauseOnHover && 'hover:[animation-play-state:paused]'
+        )}
+      >
+        {items.map((item, idx) => (
+          <li
+            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-10 md:w-[450px]"
+            style={{
+              background:
+                'linear-gradient(180deg, var(--slate-800), var(--slate-900)',
+            }}
+            key={item.id}
+          >
+            <blockquote>
+              <div className="flex items-center justify-around">
+                <span className="flex flex-col items-center justify-center text-sm text-gray-400 font-normal">
+                  <span>{item.icon}</span>
+                  <span>{item.name}</span>
+                </span>
+                <span className="flex flex-col items-center justify-center text-sm text-gray-400 font-normal">
+                  <span>{item.icon2}</span>
+                  <span>{item.name2}</span>
+                </span>
+                <span className="flex flex-col items-center justify-center text-sm text-gray-400 font-normal">
+                  <span>{item.icon3}</span>
+                  <span>{item.name3}</span>
+                </span>
+              </div>
+              {/* <div
+                aria-hidden="true"
+                className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
+              ></div>
+              <div className="relative z-20 mt-6 flex flex-row items-center">
+                <span className="flex gap-1">
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                    {item.name}
+                  </span>
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                    {item.title}
+                  </span>
+                  <span className=" text-sm leading-[1.6] text-gray-400 font-normal">
+                    {item.icon}
+                  </span>
+                </span>
+              </div> */}
+              {/* <span className=" relative z-20 text-sm leading-[1.6] text-gray-100 font-normal">
+                {item.quote}
+              </span> */}
+            </blockquote>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,222 @@
-import Image from "next/image";
+import { HeroSpot } from './Components/HeroSpot/HeroSpot';
+import { TextGenerate } from './Components/TextGenerate/TextGenerate';
+import { MovingCards } from './Components/MovingCards/MovingCards';
+import { SvgMask } from './Components/SvgMask/SvgMask';
+import { BgBeams } from './Components/BgBeams/BgBeams';
+import { BgDot } from './Components/BgDot/BgDot';
+import { Sparkles } from './Components/Sparkles/Sparkles';
+import { NavBar } from './Components/NavBar/NavBar';
+import {
+  IconHome,
+  IconMessage,
+  IconUser,
+  IconBrandReact,
+  IconBrandAngular,
+  IconBrandNodejs,
+  IconBrandCss3,
+  IconBrandHtml5,
+  IconBrandMongodb,
+  IconBrandNextjs,
+  IconBrandVscode,
+  IconAsterisk,
+  IconBrandJavascript,
+  IconBrandTypescript,
+  IconBrandRedux,
+  IconBrandFirebase,
+  IconBrandVercel,
+  IconBrandPlanetscale,
+  IconBrandGoogle,
+  IconBrandMysql,
+  IconBrandCypress,
+  IconBrandPrisma,
+  IconBrandOpenai,
+  IconBrandGithub,
+  IconBrandTailwind,
+  IconBrandGithubCopilot,
+} from '@tabler/icons-react';
+
+const Toolkit1 = [
+  {
+    id: 1,
+    icon: (
+      <IconBrandReact className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name: 'React',
+    icon2: (
+      <IconBrandAngular className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name2: 'Angular',
+    icon3: (
+      <IconBrandNextjs className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name3: 'Next.Js',
+  },
+  {
+    id: 4,
+    icon: (
+      <IconBrandJavascript className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name: 'Javascript',
+    icon2: (
+      <IconBrandTypescript className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name2: 'Typescript',
+    icon3: (
+      <IconBrandNodejs className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name3: 'Node.js',
+  },
+  {
+    id: 2,
+    icon: (
+      <IconBrandTailwind className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name: 'Tailwind',
+    icon2: (
+      <IconBrandHtml5 className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name2: 'HTML5',
+    icon3: (
+      <IconBrandCss3 className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name3: 'CSS3',
+  },
+  {
+    id: 3,
+    icon: (
+      <IconBrandOpenai className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name: 'OpenAI',
+    icon2: (
+      <IconBrandGithubCopilot className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name2: 'Copilot',
+    icon3: (
+      <IconAsterisk className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name3: 'Claude.ai',
+  },
+];
+
+const Toolkit2 = [
+  {
+    id: 1,
+    icon: (
+      <IconBrandMongodb className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name: 'MongoDB',
+    icon2: (
+      <IconBrandMysql className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name2: 'MySQL',
+    icon3: (
+      <IconBrandPlanetscale className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name3: 'PlanetScale',
+  },
+  {
+    id: 1,
+    icon: (
+      <IconBrandPrisma className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name: 'Prisma',
+    icon2: (
+      <IconBrandCypress className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name2: 'Cypress',
+    icon3: (
+      <IconBrandFirebase className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name3: 'Firebase',
+  },
+  {
+    id: 1,
+    icon: (
+      <IconBrandVercel className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name: 'Vercel',
+    icon2: (
+      <IconBrandGoogle className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name2: 'Google',
+    icon3: (
+      <IconBrandGithub className="h-10 w-10 text-neutral-500 dark:text-white" />
+    ),
+    name3: 'Github',
+  },
+];
+
+const navItems = [
+  {
+    name: 'Home',
+    link: '/',
+    icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: 'About',
+    link: '/about',
+    icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: 'Contact',
+    link: '/contact',
+    icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <section className="h-screen">
+        <NavBar navItems={navItems} />
+        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+          <HeroSpot
+            className="-top-40 left-0 md:left-60 md:-top-20 "
+            fill="white"
+          />
+          <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0 mt-40">
+            <div className="text-4xl md:text-8xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+              <span>Andres Rodriguez</span> <br />
+              <span className="text-3xl md:text-7xl ">
+                I construct the web.
+              </span>
+            </div>
+            <TextGenerate
+              words="Software engineer born in Quito, Ecuador,     constantly turning ideas into products."
+              className="mt-4 max-w-2xl text-center mx-auto"
+            ></TextGenerate>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+        {/* <BgDot /> */}
+      </section>
+      <section className="h-screen relative flex flex-col justify-items-start">
+        <BgBeams />
+        <div className="text-4xl md:text-8xl font-bold bg-clip-text text-right text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 md:m-8 md:mb-16 m-4">
+          <span>Toolkit</span> <br />
+        </div>
+        <MovingCards
+          className="mb-10"
+          items={Toolkit1}
+          direction="left"
+          speed="slow"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <MovingCards items={Toolkit2} direction="right" speed="slow" />
+      </section>
+      <section className="h-screen">
+        <SvgMask
+          revealText={
+            <p className="max-w-4xl mx-auto text-slate-800 text-center  text-4xl font-bold">
+              The first rule of MRR Club is you do not talk about MRR Club. The
+              second rule of MRR Club is you DO NOT talk about MRR Club.
+            </p>
+          }
+          className="h-[40rem] border rounded-md"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          The first rule of <span className="text-red-500">MRR Club</span> is
+          you do not talk about MRR Club. The second rule of MRR Club is you DO
+          NOT talk about <span className="text-red-500">MRR Club</span>.
+        </SvgMask>
+      </section>
+    </>
   );
 }
